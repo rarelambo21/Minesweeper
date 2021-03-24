9aeab8842657648c12f13308e09a2cca3fcf1aa2
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-24 18:54:54 +0300

    second part done

--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -20,6 +20,34 @@ int main() {
 	for (int i = 0; i < 10; i++)
 		for (int j = 0; j < 10; j++) {
 			sgrid[i][j] = 10;
+			if (rand() % 5 == 0)
+				grid[i][j] = 9;
+			else
+				grid[i][j] = 0;
+		}
+
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++) {
+			int n = 0;
+			if (grid[i][j] == 9)
+				continue;
+			if (grid[i + 1][j] == 9)
+				n++;
+			if (grid[i][j + 1] == 9)
+				n++;
+			if (grid[i - 1][j] == 9)
+				n++;
+			if (grid[i][j - 1] == 9)
+				n++;
+			if (grid[i + 1][j + 1] == 9)
+				n++;
+			if (grid[i - 1][j - 1] == 9)
+				n++;
+			if (grid[i - 1][j + 1] == 9)
+				n++;
+			if (grid[i + 1][j - 1] == 9)
+				n++;
+			grid[i][j] = n;
 		}
 
 	while (app.isOpen()) {
@@ -34,6 +62,7 @@ int main() {
 
 		for (int i = 0; i < 10; i++)
 			for (int j = 0; j < 10; j++) {
+				sgrid[i][j] = grid[i][j];
 				s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
 				s.setPosition(i * w, j * w);
 				app.draw(s);
